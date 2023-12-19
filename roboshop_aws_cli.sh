d5cc36f0e56748c6eb00fb7a1c8411428d71eeb8
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2023-12-19 15:53:59 -0600

    Ec2 instance creation using shell script

--- roboshop_aws_cli.sh
+++ roboshop_aws_cli.sh
@@ -16,8 +16,8 @@ do
     aws ec2 run-instances --image-id $img_id --instance-type $Instance_Type --security-group-ids $sg_id
   fi  
   IP_ADDRESS=$(aws ec2 run-instances --image-id $img_id --instance-type $Instance_Type --security-group-ids $sg_id --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" --query 'Instances[0].PrivateIpAddress' --output text)
-done
-<<com
+
+
   #create R53 record, make sure you delete existing record
     aws route53 change-resource-record-sets \
     --hosted-zone-id $ZONE_ID \
@@ -38,7 +38,7 @@ done
     }
         '
 done
-com
+
 
 
 
